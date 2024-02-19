2924bbf4703d28ccde3b05b08dcb2b7abfdc0e15
nylbert <nylbert.dias@gmail.com>
2024-02-18 22:05:43 -0300

    AT-04: Criar CRUD cars
    Criando service

---
+++ src/app/service/car.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from '../models/car.model';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  private url: string = 'http://localhost:8080/api/cars';
+
+  constructor(private httpClient : HttpClient, private authService: AuthService) {}
+
+  listCars() : Observable<any> {
+    let headers = this.buildHeaders();
+    return this.httpClient.get(this.url, { headers });
+  }
+
+  createCar(car: Car) : Observable<any> {
+    let headers = this.buildHeaders();
+    return this.httpClient.post(this.url, car, { headers });
+  }
+
+  findCar(id: number) : Observable<any> {
+    let headers = this.buildHeaders();
+    return this.httpClient.get(`${this.url}/${id}`, { headers });
+  }
+
+  updateCar(id: number, car: Car) : Observable<any> {
+    let headers = this.buildHeaders();
+    return this.httpClient.put(`${this.url}/${id}`, car, { headers });
+  }
+
+  deleteCar(id?: number) : Observable<any> {
+    let headers = this.buildHeaders();
+    return this.httpClient.delete(`${this.url}/${id}`, { headers });
+  }
+
+  buildHeaders() : HttpHeaders {
+    return new HttpHeaders().set('Authorization', `Bearer ${this.authService.getAuthToken}`);
+  }
+}
