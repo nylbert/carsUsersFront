caa96781537798587ae9c00ef883861df57a9356
nylbert <nylbert.dias@gmail.com>
2024-02-18 22:04:35 -0300

    AT-04: Criar CRUD cars
    Criando Componentes do CRUD

--- src/app/modules/car/car.module.ts
+++ src/app/modules/car/car.module.ts
@@ -1,11 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessagesModule } from 'primeng/messages';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { CreateCarComponent } from './create-car/create-car.component';
+import { FindCarComponent } from './find-car/find-car.component';
+import { ListCarComponent } from './list-car/list-car.component';
+import { UpdateCarComponent } from './update-car/update-car.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateCarComponent,
+    FindCarComponent,
+    ListCarComponent,
+    UpdateCarComponent
+  ],
   imports: [
+    RouterModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    MessagesModule,
+    TableModule,
+    DialogModule,
     CommonModule
   ]
 })
---
+++ src/app/modules/car/find-car/find-car.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-car',
+  templateUrl: './find-car.component.html',
+  styleUrls: ['./find-car.component.scss']
+})
+export class FindCarComponent {
+
+}
